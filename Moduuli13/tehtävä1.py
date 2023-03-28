cf97ed8639355c5dbff292dc68797a00569fb3e3
sarapappila <sara.pappila@metropolia.fi>
2023-03-28 11:03:37 +0300

    Tehty

--- Moduuli13/tehtävä1.py
+++ Moduuli13/tehtävä1.py
@@ -4,3 +4,28 @@ Hyödynnä toteutuksessa aiempaa tehtävää, jossa alkuluvun testaus tehtiin.
 Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31.
 Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
 """
+from flask import Flask, request
+app = Flask(__name__)
+
+
+def isprime(luku):
+    if luku < 2:
+        return False
+    for i in range(2, luku):
+        if luku % i == 0:
+            return False
+    return True
+
+
+@app.route('/alkuluku/<luku>')
+def alkuluku(luku):
+    luku = int(luku)
+    return {
+        "Number": luku,
+        "isPrime": isprime(luku)
+    }
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+
--- Moduuli13/tehtävä2.py
+++ Moduuli13/tehtävä2.py
@@ -5,3 +5,8 @@ Tiedot haetaan opintojaksolla käytetystä lentokenttätietokannasta.
 Esimerkiksi EFHK-koodia vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/kenttä/EFHK.
 Vastauksen on oltava muodossa: {"ICAO":"EFHK", "Name":"Helsinki Vantaa Airport", "Municipality":"Helsinki"}.
 """
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route()
+
